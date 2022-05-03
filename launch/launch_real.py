97cc3c18304418adc9f6b9bce4d12e0054ba2b0d
reyk <reyk-carstens@web.de>
2022-05-03 16:16:12 +0200

    added launch file for real robots

---
+++ launch/launch_real.py
@@ -0,0 +1,104 @@
+import os
+import sys
+import re
+import pathlib
+import xml.etree.ElementTree as ET
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+from launch.substitutions import LaunchConfiguration
+
+from lm_ros2_utils.utils import parse_launch_arguments, build_arguments_dict
+
+def get_configuration_file_path(robot_name):
+    package_dir = get_package_share_directory("lm_ros2_utils")
+    filepath = os.path.join(package_dir, "resource", robot_name + ".urdf")
+
+    return filepath
+
+def parse_urdf(filename):
+    tree = ET.parse(filename)
+    root = tree.getroot()
+
+    compass_matcher = re.compile("CompassPlugin")
+    distance_matcher = re.compile("DistancePlugin")
+    light_matcher = re.compile("LightPlugin")
+    touch_matcher = re.compile("TouchPlugin")
+
+    print(root[0][0])
+    print(root[0][0].attrib)
+
+    nodes = []
+
+    for node in root[0]:
+        plugin_name = node.attrib['type']
+        node_name = None
+        print(node)
+
+        if compass_matcher.search(plugin_name):
+            print("Compass", plugin_name)
+            node_name = "compass_sensor"
+        if distance_matcher.search(plugin_name):
+            node_name = "distance_sensor"
+        if light_matcher.search(plugin_name):
+            node_name = "light_sensor"
+        if touch_matcher.search(plugin_name):
+            node_name = "touch_sensor"
+        if re.compile("ClockPlugin").search(plugin_name):
+            continue
+
+        print(node[0], node[1])
+
+        if not node_name:
+            continue
+
+        nodes.append(
+            Node(
+                package="brickpi3_ros2",
+                executable=node_name,
+                output="screen",
+                emulate_tty=True,
+                parameters=[{
+                    "port": "PORT_A",
+                    "robot_name": "RN",
+                    "timestep": 32
+                }]
+            )
+        )
+
+    return nodes
+    
+
+
+def generate_launch_description():
+    args = parse_launch_arguments()
+    arguments_dict = build_arguments_dict(args.arguments)
+    arg_roboter_name = DeclareLaunchArgument("robot_name")
+
+    robot_name = arguments_dict["robot_name"]
+    os.environ["WEBOTS_ROBOT_NAME"] = robot_name
+
+    config_file = get_configuration_file_path(robot_name)
+    nodes = parse_urdf(config_file)
+
+    robot_description = pathlib.Path(config_file).read_text()
+    use_sim_time = LaunchConfiguration('use_sim_time', default=True)
+
+    return LaunchDescription([
+        arg_roboter_name, 
+        *nodes
+        # Node(
+        #     package='webots_ros2_driver',
+        #     executable='driver',
+        #     output='screen',
+        #     emulate_tty=True, # For showing stdout,
+        #     namespace=robot_name,
+        #     parameters=[
+        #         {'robot_description': robot_description,
+        #         'use_sim_time': use_sim_time,
+        #         'set_robot_state_publisher': True},
+        #     ],
+        # )
+    ])
